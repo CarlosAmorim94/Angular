e97cdca455c13b76d8e8bb09136877248914dcdc
carlos <carlosa@xgrow.com>
2023-09-28 18:35:29 -0300

    Learning about life circle of omponents

--- Udemy/learningAngular/src/app/app.component.ts
+++ Udemy/learningAngular/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    DoCheck,
+    AfterContentInit,
+    AfterViewInit,
+    AfterContentChecked
+{
   title = 'learningAngular';
+
+  constructor() {
+    console.log('constructor');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+    this.title = 'Novo titulo';
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  changeTitle(): void {
+    this.title += 'a';
+  }
 }
